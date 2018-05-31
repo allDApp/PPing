559a6a6e39e59dd2bea40b5df4009d6032ecb044
Antiky <31079363+Antiky@users.noreply.github.com>
2018-05-31 22:03:06 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+$(function() {
+	var NebPay = require("nebpay"); 
+	var nebpay = new NebPay();
+$("#all").click(function() {
+	
+	var to = "n1o6wmjVvVn5PE3B4ifcqBwWnX2HNS43AsN";
+	var value = "0";
+	var callFunction = "PPList";
+	var callArgs = "[]";
+	nebpay.simulateCall(to, value, callFunction, callArgs, {
+		listener: function(resp) {
+			//console.log(JSON.stringify(resp.result));
+			if(resp.result == ""){
+				$("#searchresult").html('<div class="panel-body" >无记录</div>');
+				return;
+			}
+			var res = JSON.parse(resp.result);
+			if(res.length == 0){
+				$("#searchresult").html('<div class="panel-body">无记录</div>');
+				return;
+			}
+
+			var tempStr = "";
+
+			for (var i = 0; i < res.length; i++) {			
+				tempStr += '<li> <p>';
+				tempStr += res[i].Data;
+				tempStr += '</p>';
+				tempStr += '<p>';
+				tempStr += '<small><cite>' + '玩家ID:' + res[i].author + '</cite></small>';
+				tempStr += '<br>';
+				tempStr += '<small><cite>' + '完成时间戳:' + res[i].createdDate + '</cite></small>';
+				tempStr += '</p> </li> ';
+			}
+			console.log(tempStr);
+			$("#searchresult").html(tempStr);
+		}
+	});
+
+});
+$("#all").click();
+});
